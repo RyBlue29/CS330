f3ccd56fb77139e9829c66c6403692efe83b5495
ryan <rainriot@icloud.com>
2025-09-01 18:01:10 -0400

    init

---
+++ Homework1/JavaSrc/Range.java
@@ -0,0 +1,16 @@
+public class Range {
+
+    public Range() {}
+
+    public int rangeCount(int[] A, int x, int y) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Range range = new Range();
+        int [] input = new int[10];
+        range.rangeCount(input, 0, 1);
+
+        System.out.println("Created range: " + range);
+    }
+}
